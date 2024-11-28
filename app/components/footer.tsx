a7becc7fcd2cd9a35ac9efd57519fe5551fcc336
Myungjin Ki <hello@mjki.dev>
2024-11-29 00:41:00 +0900

    Add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,3 @@
+export default function FooterComponent() {
+  return <footer className="flex items-center justify-between w-full h-32 px-8 py-4 text-xl bg-neutral-100"></footer>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import HeaderComponent from "./components/header";
+import FooterComponent from "./components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,9 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex justify-center bg-slate-300`}>
-        <div className="w-full max-w-sm bg-white h-dvh">
+        <div className="flex flex-col justify-between w-full max-w-sm bg-white h-dvh">
           <HeaderComponent />
           {children}
+          <FooterComponent />
         </div>
       </body>
     </html>
