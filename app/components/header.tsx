85608ed7e051099aca8da5bd684f210f61964430
Myungjin Ki <hello@mjki.dev>
2024-11-29 00:22:49 +0900

    Add Header

---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function HeaderComponent() {
+  return (
+    <header className="flex justify-between w-full px-8 py-4 text-xl bg-neutral-100">
+      <div>추가</div>
+      <h1>외워져라 일본어!</h1>
+      <div>검색</div>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import HeaderComponent from "./components/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,8 +15,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Flashvoca Japanese",
-  description: "Flashvoca Japanese",
+  title: "외워져라 일본어!",
+  description: "외워져라 일본어!",
 };
 
 export default function RootLayout({
@@ -25,7 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex justify-center bg-slate-300`}>
+        <div className="w-full max-w-sm bg-white h-dvh">
+          <HeaderComponent />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
