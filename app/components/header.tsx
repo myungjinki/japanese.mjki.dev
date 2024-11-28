b3fee4b2bc9f766e36d3edea53347e6c20118791
Myungjin Ki <hello@mjki.dev>
2024-11-29 00:41:06 +0900

    Modify Header CSS

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,6 @@
 export default function HeaderComponent() {
   return (
-    <header className="flex justify-between w-full px-8 py-4 text-xl bg-neutral-100">
+    <header className="flex items-center justify-between w-full h-16 px-8 py-4 text-xl bg-neutral-100">
       <div>추가</div>
       <h1>외워져라 일본어!</h1>
       <div>검색</div>
